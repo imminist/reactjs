9329fe41a9d3d4e6feb58be3d8b41c0c4ed0c72a
Hiro Nguyen <78646685+imminist@users.noreply.github.com>
2023-02-24 00:49:26 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MainNavigation from './layout/MainNavigation'
 function App() {
   return (
     <div>
+      <h1>Hello</h1>
       <BrowserRouter>
         <MainNavigation />
         <Routes>
